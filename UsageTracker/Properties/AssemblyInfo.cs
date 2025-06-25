a67ab6889dfba5750992dec767f540a6c25f397f
Anonymity <331433038@qq.com>
2025-06-25 20:57:13 +0800

    修改项目配置

--- UsageTracker/Properties/AssemblyInfo.cs
+++ UsageTracker/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("UsageTracker")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("记录Visual Studio使用情况")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Anonymity")]
 [assembly: AssemblyProduct("UsageTracker")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
